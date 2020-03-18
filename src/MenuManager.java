9bf11fe82fb893c8f4c06aee03ea4d92d565e2f2
happysansam <zksi67@gmail.com>
2020-03-18 18:24:47 +0900

    Cash to History

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -4,7 +4,7 @@ public class MenuManager {
 		System.out.println("1. Spending");
 		System.out.println("2. Income");
 		System.out.println("3. Balance");
-		System.out.println("4. Cash");
+		System.out.println("4. History");
 		System.out.println("5. Exit");
 		System.out.println("Select ont number between 1 - 5:");
 	}
