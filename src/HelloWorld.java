a7388dbf8bc939ef459fc67d56e08841b9c8cb2e
happysansam <zksi67@gmail.com>
2020-03-18 15:09:51 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("*** Student Management Sysytem Menu");
+		System.out.println("1. Add Student");
+		System.out.println("2. Delete Sutdent");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("5. Exit");
+		System.out.println("Select ont number between 1 - 5:");
+	}
+}
