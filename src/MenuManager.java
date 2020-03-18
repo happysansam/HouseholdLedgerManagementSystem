b38dcf53dfe84143d10bfbfbf1aee2a317cca7db
happysansam <zksi67@gmail.com>
2020-03-18 19:17:33 +0900

    Make Spending class and join together.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class MenuManager {
 	public static void main(String[] args) {
+		Spending name;
+		name = new Spending();
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("*** Householde Ledger Management Sysytem Menu");
 		System.out.println("1. Spending");
 		System.out.println("2. Income");
@@ -7,5 +13,11 @@ public class MenuManager {
 		System.out.println("4. History");
 		System.out.println("5. Exit");
 		System.out.println("Select ont number between 1 - 5:");
+		
+		int a = sc.nextInt();
+		
+		if (a==1){
+			name.input();
+		}
 	}
 }
