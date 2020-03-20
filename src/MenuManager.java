9d40ca0112ddb282e3edb1ca25d0aefc3a6fc5e7
happysansam <zksi67@gmail.com>
2020-03-20 19:49:44 +0900

    Correct issue about MenuManager case5.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class MenuManager {
 	
 	public static int num=0;
-	public static int Balance=0;
+	public static int Balance;
 	public static int Spendingsp[]=new int[999];
 	public static String SpendinghowS[]=new String[999];
 	public static int Incomeic[]=new int[999];
@@ -14,41 +14,44 @@ public class MenuManager {
 	static HistoryList HistoryOne= new HistoryList();
 	
 	public static void main(String[] args) {
+		while(true) {
+			if (num!=0) {
+				BalanceOne.input();
+			}
 		
-		int a=0;
-		Balance = 0;
-		sc = new Scanner(System.in);
+			int a=0;
+			Balance = 0;
 		
-		System.out.println("*** Householde Ledger Management Sysytem Menu\n1. Spending\n2. Income\n3. Balance\n4. History\n5. Exit\nSelect one number between 1 - 5:");
+			System.out.println("*** Householde Ledger Management Sysytem Menu\n1. Spending\n2. Income\n3. Balance\n4. History\n5. Exit\nSelect one number between 1 - 5:");
 		
-		a = sc.nextInt();
-		switch(a) {
-		default :
-			System.out.println("There's a problem. Please select one number between 1 - 5. Going back to starting point.");
-			main(args);
-		case 1:
-			SpendingOne.input();
-			Spendingsp[num]=SpendingOne.sp;
-			SpendinghowS[num]=SpendingOne.howS;
-			num++;
-			BalanceOne.input();
-			main(args);
-		case 2:
-			IncomeOne.input();
-			Incomeic[num]=IncomeOne.ic;
-			IncomehowS[num]=IncomeOne.howS;
-			num++;
-			BalanceOne.input();
-			main(args);
-		case 3:
-			BalanceOne.input();
-			main(args);
-		case 4:
-			HistoryOne.input();
-			main(args);
-		case 5:
-			break;
+			sc = new Scanner(System.in);
+			a = sc.nextInt();
+		
+			switch(a) {
+			default :
+				System.out.println("There's a problem. Please select one number between 1 - 5. Going back to starting point.");
+				continue;
+			case 1:
+				SpendingOne.input();
+				Spendingsp[num]=SpendingList.sp;
+				SpendinghowS[num]=SpendingList.howS;
+				num++;
+				continue;
+			case 2:
+				IncomeOne.input();
+				Incomeic[num]=IncomeList.ic;
+				IncomehowS[num]=IncomeList.howS;
+				num++;
+				continue;
+			case 3:
+				main(args);
+			case 4:
+				HistoryOne.input();
+				continue;
+			case 5:
+				break;
+			}
+		break;
 		}
-		sc.close();
 	}
 }
