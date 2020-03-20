d1b6871ad61397510e6cb30357b3b46fdb25604a
happysansam <zksi67@gmail.com>
2020-03-20 10:45:02 +0900

    Just All the system.

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,21 +1,49 @@
 import java.util.Scanner;
 public class MenuManager {
+	
+	public static int num=0;
+	public static int Balance=0;
+	public static int Spendingsp[]=new int[999];
+	public static String SpendinghowS[]=new String[999];
+	public static int Incomeic[]=new int[999];
+	public static String IncomehowS[]=new String[999];
+	static Scanner sc; 
+	static SpendingList SpendingOne = new SpendingList();
+	static IncomeList IncomeOne = new IncomeList();
+	static Balance BalanceOne = new Balance();
+	static HistoryList HistoryOne= new HistoryList();
+	
 	public static void main(String[] args) {
-		Spending name;
-		name = new Spending();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("*** Householde Ledger Management Sysytem Menu");
-		System.out.println("1. Spending");
-		System.out.println("2. Income");
-		System.out.println("3. Balance");
-		System.out.println("4. History");
-		System.out.println("5. Exit");
-		System.out.println("Select ont number between 1 - 5:");
 		
-		int a = sc.nextInt();
+		int a=0;
+		sc = new Scanner(System.in);
 		
-		if (a==1){
-			name.input();
+		System.out.println("*** Householde Ledger Management Sysytem Menu\n1. Spending\n2. Income\n3. Balance\n4. History\n5. Exit\nSelect one number between 1 - 5:");
+		
+		a = sc.nextInt();
+		switch(a) {
+		default :
+			System.out.println("There's a problem. Please select one number between 1 - 5. Going back to starting point.");
+			main(args);
+		case 1:
+			SpendingOne.input();
+			Spendingsp[num]=SpendingOne.sp;
+			SpendinghowS[num]=SpendingOne.howS;
+			main(args);
+		case 2:
+			IncomeOne.input();
+			Incomeic[num]=IncomeOne.ic;
+			IncomehowS[num]=IncomeOne.howS;
+			main(args);
+		case 3:
+			BalanceOne.input();
+			main(args);
+		case 4:
+			HistoryOne.input();
+			main(args);
+		case 5:
+			break;
 		}
+		sc.close();
 	}
 }
---
+++ src/SpendingList.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SpendingList {
+	
+	public static int sp;
+	public static String howS;
+	static Scanner spsc;
+	static Scanner sch;
+	
+	public void input() {
+		spsc = new Scanner(System.in);
+		sch = new Scanner(System.in);
+		
+		System.out.println("Please input spending amount :");
+		sp = spsc.nextInt();
+		
+		System.out.println("How :");
+		howS = sch.nextLine();
+		
+		System.out.printf("%dwon, to %s\n", sp, howS);
+		MenuManager.num++;
+		MenuManager.BalanceOne.input();
+	}
+}
